04cf3d04d7f7653e7db3c20170707287b9a2867f
venzovisa <nazobvane@gmail.com>
2020-06-27 17:22:50 +0300

    Added change user password feature

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const auth = require('../middleware/auth');
 require('express-async-errors');
 
@@ -34,6 +36,44 @@ route_user.post('/register', jsonParser, async (req, res) => {
         console.log(ex.message);
         res.status(400).end();
     }
+});
+
+route_user.post('/user', jsonParser, async (req, res) => {
+    const token = req.header('Auth-Token');
+    if (!token) return (res.status(401).send('Access token required'));
+    let email;
+    jwt.verify(token, config.get('jwtPrivateKey'), function(err, decoded) {
+        if (err) res.status(400).send('Invalid token.');
+        email = decoded.email;
+    });
+
+    //
+    // const { error } = validateUser(req.body);
+    // if (error) return res.status(400).send(error.details[0].message);
+    //
+     try {
+        let user = await User.findOne({email: email});
+        if(!user) return res.status(400).send('User not existing');
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if(!validPassword) return res.status(400).send('Invalid password');
+
+        if (req.body.password === req.body.passwordNew) return res.status(400).send('Current and new password cannot be the same');
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(req.body.passwordNew, salt);
+        const result = await user.save();
+        const token = user.generateAuthToken();
+        res
+            .header('Auth-Token', token)
+            .header('access-control-expose-headers', 'Auth-Token')
+            .send(_.pick(user, ['_id', 'name', 'email']));
+         res.status(200).send('Password changed').end();
+ }
+   catch(ex){
+        console.log(ex.message);
+        res.status(400).end();
+   }
 });
 
 module.exports = route_user;
