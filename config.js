367ebe13832e6fffceb846abb739351885006c40
Venzo <nazobvane@gmail.com>
2021-06-08 16:56:59 +0300

    Removed DB test user

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-process.env.db_url = 'mongodb+srv://test:tCXkq9FcQt9Ptw9x@cluster0.pj3bi.mongodb.net/db_store?retryWrites=true&w=majority'
 process.env.db_store = 'db_store';
 process.env.db_store_orders = 'db_orders';
 process.env.db_store_products = 'db_products';
